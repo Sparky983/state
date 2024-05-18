2ccad28066ca22f445d24f1f3df28231e702c7b8
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2024-05-18 18:45:43 +1000

    Configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `java-library`
     alias(libs.plugins.spotless)
+    alias(libs.plugins.maven.publish)
 }
 
 repositories {
@@ -22,6 +27,38 @@ spotless {
     java {
         palantirJavaFormat().style("GOOGLE")
         formatAnnotations()
+    }
+}
+
+mavenPublishing {
+    configure(JavaLibrary(
+        javadocJar = JavadocJar.Javadoc(),
+        sourcesJar = true,
+    ))
+    signAllPublications()
+    if (!version.toString().endsWith("-SNAPSHOT")) {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    }
+
+    pom {
+        description.set(project.description ?: throw IllegalStateException("Add a project description"))
+        url.set("https://github.com/Sparky983/state/")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://www.opensource.org/licenses/mit-license")
+            }
+        }
+        developers {
+            developer {
+                id.set("Sparky983")
+            }
+        }
+        scm {
+            url.set("https://github.com/Sparky983/state/")
+            connection.set("scm:git:git://github.com/Sparky983/state.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Sparky983/state.git")
+        }
     }
 }
 
